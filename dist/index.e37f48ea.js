d36c4a2ebd41defde846a78fc246d6953f604e45
Zerko3 <zan.zerak99@gmail.com>
2023-04-26 21:22:54 +0200

    bug

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -600,7 +600,7 @@ const init = function() {
 };
 init();
 
-},{"./model.js":"Y4A21","./view.js":"ky8MP","./views/addAsideMarkup.js":"4wDdN","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","./views/addHighlights.js":"gKboZ"}],"Y4A21":[function(require,module,exports) {
+},{"./model.js":"Y4A21","./view.js":"ky8MP","./views/addAsideMarkup.js":"4wDdN","./views/addHighlights.js":"gKboZ","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"Y4A21":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);
 parcelHelpers.export(exports, "state", ()=>state);
