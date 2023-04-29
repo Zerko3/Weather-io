acb5fe1d6a58a4811ab450d7ee3d27ba5bed2770
Zerko3 <zan.zerak99@gmail.com>
2023-04-29 15:31:09 +0200

    refectored js

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -855,79 +855,35 @@ var _view = require("../view");
 var _viewDefault = parcelHelpers.interopDefault(_view);
 class AddDaysOfTheWeek extends (0, _viewDefault.default) {
     _weekDaysElement = document.querySelector(".weather-section--weather__display--box");
+    _date = new Date();
+    _daysInAWeek = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
     constructor(){
         super();
     }
-    // TODO:
-    // 1. for each in generijraj markup
     generateMarkupWeekDays(data) {
-        const html = `
-    <div
-    class="weather-section--weather__display--box--weather__day--box">
-    <h2>Monday</h2>
-    <i class="fa-solid fa-cloud"></i>
-    <div class="weather__day__morning--num--box">
-      <span class="weather__day__morning--num">${data.daysInTheWeek[1].day.mintemp_c}&#8451;</span>
-      <span class="weather__day__evening--num">${data.daysInTheWeek[1].day.maxtemp_c}&#8451;</span>
-    </div>
-  </div>
-
-  <div
-  class="weather-section--weather__display--box--weather__day--box">
-  <h2>Tuesday</h2>
-  <i class="fa-solid fa-cloud"></i>
-  <div class="weather__day__morning--num--box">
-    <span class="weather__day__morning--num">${data.daysInTheWeek[2].day.mintemp_c}&#8451;</span>
-    <span class="weather__day__evening--num">${data.daysInTheWeek[2].day.maxtemp_c}&#8451;</span>
-  </div>
-</div>
-<div
-  class="weather-section--weather__display--box--weather__day--box">
-  <h2>Wednesday</h2>
-  <i class="fa-solid fa-cloud"></i>
-  <div class="weather__day__morning--num--box">
-    <span class="weather__day__morning--num">${data.daysInTheWeek[3].day.mintemp_c}&#8451;</span>
-    <span class="weather__day__evening--num">${data.daysInTheWeek[3].day.maxtemp_c}&#8451;</span>
-  </div>
-</div>
-<div
-  class="weather-section--weather__display--box--weather__day--box">
-  <h2>Thursday</h2>
-  <i class="fa-solid fa-cloud"></i>
-  <div class="weather__day__morning--num--box">
-  <span class="weather__day__morning--num">${data.daysInTheWeek[4].day.mintemp_c}&#8451;</span>
-  <span class="weather__day__evening--num">${data.daysInTheWeek[4].day.maxtemp_c}&#8451;</span>
-  </div>
-</div>
-<div
-  class="weather-section--weather__display--box--weather__day--box">
-  <h2>Friday</h2>
-  <i class="fa-solid fa-cloud"></i>
-  <div class="weather__day__morning--num--box">
-  <span class="weather__day__morning--num">${data.daysInTheWeek[5].day.mintemp_c}&#8451;</span>
-  <span class="weather__day__evening--num">${data.daysInTheWeek[5].day.maxtemp_c}&#8451;</span>
-  </div>
-</div>
-<div
-  class="weather-section--weather__display--box--weather__day--box">
-  <h2>Saturday</h2>
-  <i class="fa-solid fa-cloud"></i>
-  <div class="weather__day__morning--num--box">
-  <span class="weather__day__morning--num">${data.daysInTheWeek[6].day.mintemp_c}&#8451;</span>
-  <span class="weather__day__evening--num">${data.daysInTheWeek[6].day.maxtemp_c}&#8451;</span>
-  </div>
-</div>
-<div
-  class="weather-section--weather__display--box--weather__day--box">
-  <h2>Sunday</h2>
-  <i class="fa-solid fa-cloud"></i>
-  <div class="weather__day__morning--num--box">
-  <span class="weather__day__morning--num">${data.daysInTheWeek[0].day.mintemp_c}&#8451;</span>
-  <span class="weather__day__evening--num">${data.daysInTheWeek[0].day.maxtemp_c}&#8451;</span>
-  </div>
-</div>
-    `;
-        this._weekDaysElement.insertAdjacentHTML("afterbegin", html);
+        data.daysInTheWeek.forEach((element, index)=>{
+            console.log(element, index);
+            const html = `
+        <div
+        class="weather-section--weather__display--box--weather__day--box">
+        <h2>${this._daysInAWeek[index]}</h2>
+        <i class="fa-solid fa-cloud"></i>
+        <div class="weather__day__morning--num--box">
+          <span class="weather__day__morning--num">${element.day.mintemp_c}&#8451;</span>
+          <span class="weather__day__evening--num">${element.day.maxtemp_c}&#8451;</span>
+        </div>
+      </div>`;
+            console.log(html);
+            this._weekDaysElement.insertAdjacentHTML("afterbegin", html);
+        });
     }
 }
 exports.default = new AddDaysOfTheWeek();
